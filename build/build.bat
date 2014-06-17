172632b1170185c3892d3cd5e9ab908ab9e50686
Anders Erlandsson <sbkkae@WB01764.personal.sundsvall.se>
2014-06-17 14:28:07 +0200

    Cleaning up resource stored double. Resources are stored in repo root /resources Updated integration files (.html), readme.md and build scripts

--- build/build.bat
+++ build/build.bat
@@ -1,16 +1,18 @@
 @echo off 
-set release_name=OpenEMap-1.0.4.13
+set release_name=OpenEMap-1.0.4.17
 echo ..\release\%release_name%
 cd ..\release
+rd /s /q %release_name%
+del %release_name%.zip
 md %release_name%
 cd %release_name%
 md config
 md resources
 cd..
-sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,\libs\geoext-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat --closure ..\OpenEMap-all.js
-sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,\libs\geoext-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat ..\OpenEMap-debug-all.js
+sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,\libs\geoext-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat --closure ..\%release_name%-all.js
+sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,\libs\geoext-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat ..\%release_name%-debug-all.js
 copy ..\*.html ..\release\%release_name%
-copy ..\OpenEMap-all.js ..\release\%release_name%\OpenEMap-all.js
+copy ..\*.js ..\release\%release_name%
 xcopy ..\config ..\release\%release_name%\config /E
 xcopy ..\resources ..\release\%release_name%\resources /E
 zip -r %release_name%.zip %release_name%/*
