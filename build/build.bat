16134034a39a80cbbb6b07c720675b18ef23c174
Anders Erlandsson <sbkkae@WB01764.personal.sundsvall.se>
2014-06-19 13:50:37 +0200

    bulid script update - excluding debug script

--- build/build.bat
+++ build/build.bat
@@ -10,9 +10,9 @@ md config
 md resources
 cd..
 sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,\libs\geoext-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat --closure ..\%release_name%-all.js
-sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,\libs\geoext-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat ..\%release_name%-debug-all.js
+sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,\libs\geoext-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat ..\%release_name%-all-debug.js
 copy ..\*.html ..\release\%release_name%
-copy ..\*.js ..\release\%release_name%
+copy ..\*-all.js ..\release\%release_name%
 xcopy ..\config ..\release\%release_name%\config /E
 xcopy ..\resources ..\release\%release_name%\resources /E
 zip -r %release_name%.zip %release_name%/*
