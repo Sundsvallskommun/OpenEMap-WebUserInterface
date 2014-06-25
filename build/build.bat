f9d1716e5ca2d80796ade6b43be3f05da7ad79b3
Anders Erlandsson <sbkkae@WB01764.personal.sundsvall.se>
2014-06-25 08:13:46 +0200

    Fix bug in build.bat script

--- build/build.bat
+++ build/build.bat
@@ -1,16 +1,18 @@
 @echo off 
-set release_name=OpenEMap-1.0.4.17
+set release_name=OpenEMap-1.1.0
 echo ..\release\%release_name%
 cd ..\release
 rd /s /q %release_name%
 del %release_name%.zip
+del ..\%release_name%-all.js
+del ..\%release_name%-all-debug.js
 md %release_name%
 cd %release_name%
 md config
 md resources
 cd..
-sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,\libs\geoext-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat --closure ..\%release_name%-all.js
-sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,\libs\geoext-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat ..\%release_name%-all-debug.js
+sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,..\..\libs\geoext2-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat --closure ..\%release_name%-all.js
+sencha.exe compile --classpath=..\src\main\javascript,\libs\ext-4.2.1\src,..\..\libs\geoext2-2.0.1\src exclude -all and include -namespace OpenEMap and include -file ..\src\main\javascript\OpenEMap.js and concat ..\%release_name%-all-debug.js
 copy ..\*.html ..\release\%release_name%
 copy ..\*-all.js ..\release\%release_name%
 xcopy ..\config ..\release\%release_name%\config /E
