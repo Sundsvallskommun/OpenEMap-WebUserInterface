eb795edd84827a4a456f8258e3705859a45c6262
Anders Erlandsson <anders.erlandsson@sundsvall.se>
2014-08-29 11:27:36 +0200

    Show coordinates in map view. TODO - move rendering to div.

--- src/main/javascript/view/Map.js
+++ src/main/javascript/view/Map.js
@@ -159,6 +159,16 @@ Ext.define('OpenEMap.view.Map' ,{
         
         this.selectControl = new OpenLayers.Control.SelectFeature(this.drawLayer);
         this.map.addControl(this.selectControl);
+
+        // Show coordinates in lower right corner of map
+        this.map.addControl(
+            new OpenLayers.Control.MousePosition({
+                prefix: '',
+                separator: ' | ',
+                numDigits: 2,
+                emptyString: 'Mouse is not over map.'
+            })
+        );
     },
     unselectAll: function() {
         this.drawLayer.selectedFeatures.forEach(function(feature) {
