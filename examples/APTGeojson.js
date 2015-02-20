0c2bb7cdba5788699cc34bd4e3df7621e2617a1e
Anders Erlandsson <anders.erlandsson@sundsvall.se>
2015-02-20 11:15:34 +0100

    APT example

---
+++ examples/APTGeojson.js
@@ -0,0 +1,66 @@
+/*    
+    Copyright (C) 2014 Härnösands kommun
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @author Anders Erlandsson, Sundsvalls kommun
+ */
+var geojson = '';
+geojson += '{';
+geojson += '  "type": "FeatureCollection",';
+geojson += '  "features": ';
+geojson += '  [';
+geojson += '    { ';
+geojson += '      "type": "Feature",';
+geojson += '      "geometry": {"type": "Point", "coordinates": [619676.0, 6917207.0]},';
+geojson += '      "properties":';
+geojson += '      {';
+geojson += '        "id": 1,';
+geojson += '        "popupTitle": "Omklädning",';
+geojson += '        "popupText": "Omklädningsrum"';
+geojson += '      }';
+geojson += '    },';
+geojson += '    { ';
+geojson += '      "type": "Feature",';
+geojson += '      "geometry": {"type": "Point", "coordinates": [619478.0, 6917159.0]},';
+geojson += '      "properties":';
+geojson += '      {';
+geojson += '        "id": 2,';
+geojson += '        "popupTitle": "Raststuga",';
+geojson += '        "popupText": "Raststuga"';
+geojson += '      }';
+geojson += '    },';
+geojson += '    { ';
+geojson += '      "type": "Feature",';
+geojson += '      "geometry": {"type": "Point", "coordinates": [619627.0, 6917237.0]},';
+geojson += '      "properties":';
+geojson += '      {';
+geojson += '        "id": 3,';
+geojson += '        "popupTitle": "Parkering",';
+geojson += '        "popupText": "Parkering"';
+geojson += '      }';
+geojson += '    },';
+geojson += '    { ';
+geojson += '      "type": "Feature",';
+geojson += '      "geometry": {"type": "Point", "coordinates": [619384.0, 6917131.0]},';
+geojson += '      "properties":';
+geojson += '      {';
+geojson += '        "id": 4,';
+geojson += '        "popupTitle": "Skidspår",';
+geojson += '        "popupText": "Skidspår"';
+geojson += '      }';
+geojson += '    }';
+geojson += '  ]';
+geojson += '}';
