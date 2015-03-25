ad292308942829a29c5a995e05d27d1b6a538e66
Anders Erlandsson <anders.erlandsson@sundsvall.se>
2015-03-25 09:20:33 +0100

    Release 1.6.0-rc.3 updates in integration files

--- build/change_integrationfiles_javascriptname.bat
+++ build/change_integrationfiles_javascriptname.bat
@@ -1,4 +1,4 @@
-fart -c -- ..\*.html OpenEMap-1.5.0 OpenEMap-1.6.0-rc.2
-fart -r -c -- ..\dev\*.html OpenEMap-1.5.0 OpenEMap-1.6.0-rc.2
-fart -r -c -- ..\examples\*.html OpenEMap-1.5.0 OpenEMap-1.6.0-rc.2
+fart -c -- ..\*.html OpenEMap-1.6.0-rc.2 OpenEMap-1.6.0-rc.3
+fart -r -c -- ..\dev\*.html OpenEMap-1.6.0-rc.2 OpenEMap-1.6.0-rc.3
+fart -r -c -- ..\examples\*.html OpenEMap-1.6.0-rc.2 OpenEMap-1.6.0-rc.3
 pause
--- src/main/javascript/Client.js
+++ src/main/javascript/Client.js
@@ -28,7 +28,7 @@ Ext.define('OpenEMap.Client', {
                'OpenEMap.view.PopupResults',
                'OpenEMap.OpenLayers.Control.ModifyFeature',
                'OpenEMap.OpenLayers.Control.DynamicMeasure'],
-    version: '1.6.0-rc.1',
+    version: '1.6.0-rc.3',
     /**
      * OpenLayers Map instance
      * 
