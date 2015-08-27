7cdde27d0eb200b56e97d7d7e233c1c2644622a6
Anders Erlandsson <anders.erlandsson@sundsvall.se>
2015-08-27 11:37:30 +0200

    Updates to dev files

--- Gruntfile.js
+++ Gruntfile.js
@@ -186,11 +186,6 @@ module.exports = function(grunt) {
             https: true,
             port: 443
         }, {
-            context: '/print',
-            host: 'kartatest.e-tjansteportalen.se',
-            https: true,
-            port: 443
-        }, {
             context: '/mapfishprint-2.1.0',
             host: 'kartatest.e-tjansteportalen.se',
             https: true,
